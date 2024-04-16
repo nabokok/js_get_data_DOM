2b9a3b58f3ecd9c66043d436d62d7d4d5c4515d4
Alyona Nabok <alyonana42@gmail.com>
2024-04-16 17:18:56 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const population = [
+    ...document.querySelectorAll(".population")
+].map((span)=>+span.textContent.replace(/[,!]/g, ""));
+const totalPopulationSpan = document.querySelector(".total-population");
+const averagePopulationSpan = document.querySelector(".average-population");
+const totalPopulation = population.reduce((cur, acc)=>cur + acc);
+const averagePopulation = totalPopulation / population.length;
+totalPopulationSpan.textContent = totalPopulation.toLocaleString("en-US");
+averagePopulationSpan.textContent = averagePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
